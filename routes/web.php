460d482dc35c46d83e0d2f48d3ae48bca21d3587
江口拓弥 <takuya@Takuya-MacBook.local>
2018-01-26 23:09:37 +0900

    recommit

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,23 @@ Route::get('/', function () {
 Route::get('hello', function () {
     return '<html><body><h1>Hello</h1><p>This is sample</p></body></html>';
 });
+
+
+Route::get('hello2/{msg?}', function ($msg = 'hoge') { 
+  $html = <<< EOM
+  <html>
+  <head>
+  <style>
+  body {font-size:16pt; color:#999;}
+  h1 {font-size: 100pt; text-align:right; color:#eee; margin:-40px 0px -50px 0px;}
+  </style>
+  </head>
+  <body>
+    <h1>Hello</h1>
+    <p>{$msg}</p>
+    <p>これはサンプルで作ったページです。</p>
+  </body>
+  </html>
+EOM;
+  return $html;
+});
