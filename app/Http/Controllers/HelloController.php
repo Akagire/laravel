bc0325f6d1d3991a03b41aaeccf5d3925ef16f5d
江口拓弥 <takuya@Takuya-MacBook.local>
2018-01-26 23:46:10 +0900

    Controller

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -4,23 +4,44 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+global $head, $style, $body, $end;
+$head = '<html><head>';
+$style = <<<EOM
+<style>
+body {
+  font-size:16pt; color:#999;
+}
+h1 {
+  font-size: 100pt;
+  text-align:right;
+  color:#eee;
+  margin:-40px 0px -50px 0px;
+}
+</style>
+EOM;
+$body = '</head><body>';
+$end = '</body></html>';
+
+function tag($tag, $txt) {
+  return "<$tag>" . $txt . "</$tag>";
+}
+
 class HelloController extends Controller
 {
     //
     public function index() {
-      return <<< EOF
-<html>
-<head>
-<title>Hello/Index</title>
-<style>
-body {font-size:16pt; color:#999;}
-h1 {font-size: 100pt; text-align:right; color:#eee; margin:-40px 0px -50px 0px;}
-</style>
-</head>
-<body>
-<h1>Index</h1>
-<p>これは、HelloControllerのindexアクションです。</p>
-</html>
-EOF;
+      global $head, $style, $body, $end;
+      $html = $head . tag('title', 'Hello/Index') . $style . $body .
+        tag('h1', 'Index') . tag('p', 'This is index page') .
+        '<a href="/hello/other">go to Other Page</a>' . $end;
+      return $html;
+    }
+
+    public function other() {
+      global $head, $style, $body, $end;
+      $html = $head . tag('title', 'Hello/Other') . $style . $body .
+        tag('h1', 'Other') . tag('p', 'This is other page') .
+        '<a href="/hello">go to index Page</a>' . $end;
+      return $html;
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -15,28 +15,5 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('hello', function () {
-    return '<html><body><h1>Hello</h1><p>This is sample</p></body></html>';
-});
-
-
-Route::get('hello2/{msg?}', function ($msg = 'hoge') {
-  $html = <<< EOM
-  <html>
-  <head>
-  <style>
-  body {font-size:16pt; color:#999;}
-  h1 {font-size: 100pt; text-align:right; color:#eee; margin:-40px 0px -50px 0px;}
-  </style>
-  </head>
-  <body>
-    <h1>Hello</h1>
-    <p>{$msg}</p>
-    <p>これはサンプルで作ったページです。</p>
-  </body>
-  </html>
-EOM;
-  return $html;
-});
-
-Route::get('hello3', 'HelloController@index');
+Route::get('hello', 'HelloController@index');
+Route::get('hello/other', 'HelloController@other');
