c48db8289568d19cfeef5915e567e48c20fd5d39
江口拓弥 <takuya@Takuya-MacBook.local>
2018-01-27 14:08:23 +0900

    Blade test

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -3,10 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+USE Illuminate\Http\Response;
 
-global $head, $style, $body, $end;
-$head = '<html><head>';
-$style = <<<EOM
+class HelloController extends Controller
+{
+    public function index(Request $request ,Response $response) {
+      $response->setContent('hoge');
+      $html = <<<EOF
+<html>
+<head>
+<title>Hello</title>
 <style>
 body {
   font-size:16pt; color:#999;
@@ -18,30 +24,17 @@ h1 {
   margin:-40px 0px -50px 0px;
 }
 </style>
-EOM;
-$body = '</head><body>';
-$end = '</body></html>';
-
-function tag($tag, $txt) {
-  return "<$tag>" . $txt . "</$tag>";
-}
-
-class HelloController extends Controller
-{
-    //
-    public function index() {
-      global $head, $style, $body, $end;
-      $html = $head . tag('title', 'Hello/Index') . $style . $body .
-        tag('h1', 'Index') . tag('p', 'This is index page') .
-        '<a href="/hello/other">go to Other Page</a>' . $end;
-      return $html;
-    }
-
-    public function other() {
-      global $head, $style, $body, $end;
-      $html = $head . tag('title', 'Hello/Other') . $style . $body .
-        tag('h1', 'Other') . tag('p', 'This is other page') .
-        '<a href="/hello">go to index Page</a>' . $end;
-      return $html;
+</head>
+<body>
+<h1>Hello</h1>
+<h3>Request<h3>
+<pre>{$request}</pre>
+<h3>Response</h3>
+<pre>{$response->content()}</pre>
+</body>
+</html>
+EOF;
+      $response->setContent($html);
+      return $response;
     }
 }
